a76a60866e479b94898c7dfdb235931cecd7076c
Geetha <danda.saigeetha@wipro.com>
2025-05-22 13:37:27 +0000

    Fix non-breaking space issue

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
-
 public class HelloJenkins {
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins");
-    }
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
 }
+
